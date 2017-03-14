48a122f87b96484724e60af039d80c6937a71122
Edward Ward <edwardwardward@gmail.com>
2017-03-14 16:50:15 +0000

    completed ex 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,23 @@
 class DockingStation
-  def release_bike
-    return Bike.new
+
+  attr_reader :bikes
+
+  def initialize()
+    @bikes = []
+    @capacity = 20
   end
+
+  def release_bike
+    if @bikes == []
+      fail
+    else
+      @bikes.pop
+    end
+  end
+
+  def dock_bike(bike)
+    return fail 'Docking station full' if @bikes.size >= @capacity
+    @bikes.push(bike)
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,36 @@ require 'bike'
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
-  it 'should instantiate the bike class' do
-    expect(subject.release_bike).to be_a Bike
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
-  it 'should be working' do
-    bike = subject.release_bike
+
+  it 'should produce a working bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
     expect(subject.release_bike.working?).to be true
   end
+
+  it 'check dock_bikes method adds to @bikes array' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.instance_variable_get(:@bikes)).to include(bike)
+  end
+
+  it 'raises an error when there are no bikes available' do
+    if (subject.instance_variable_get(:@bikes)) == []
+      expect {subject.release_bike}.to raise_error
+    end
+  end
+
+  it 'raises an error when bikes are going over capacity' do
+    (subject.instance_variable_get(:@capacity)).times {subject.dock_bike Bike.new}
+    expect {subject.dock_bike Bike.new}.to raise_error 'Docking station full'
+  end
+
 end
